984def24aa63210fef7dd0eaf5636d9e5a42ec4e
panqiutang <36254080+panqiutang@users.noreply.github.com>
2018-04-13 04:20:23 +0000

    4/12 tcp connection

--- client.c
+++ client.c
@@ -1,27 +1,50 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
 #include <sys/socket.h>
-#include <bluetooth/bluetooth.h>
-#include <bluetooth/rfcomm.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 int main(int argc, char **argv)
 {
-struct sockaddr_rc addr = { 0 };
-int s, status;
-//char dest[18] = "B8:27:EB:A8:97:01"; //raspberrypi-1
-char dest[18] = "A4:5E:60:C5:38:E6"; // zxm macbook
-// allocate a socket
-s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
-// set the connection parameters (who to connect to)
-addr.rc_family = AF_BLUETOOTH;
-addr.rc_channel = (uint8_t) 1;
-str2ba( dest, &addr.rc_bdaddr );
-// connect to server
-status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
-// send a message
-if( status == 0 ) {
-status = write(s, "hello!", 6);
-}
-if( status < 0 ) perror("uh oh");
-close(s);
-return 0;
+	int s;
+	int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+	struct addrinfo hints, *result;
+	memset(&hints, 0, sizeof(struct addrinfo));
+	hints.ai_family = AF_INET; /* IPv4 only */
+	hints.ai_socktype = SOCK_STREAM;
+
+	s = getaddrinfo("192.168.1.101", "1234", &hints, &result);
+	if (s != 0) {
+	        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+        	exit(1);
+	}
+/*
+	struct sockaddr_in localaddr;
+	localaddr.ai_family = AF_INET;
+	localaddr.ai_addr.s_addr = inet_addr("192.168.1.101");
+	localaddr.ai_port = 1234;
+*/
+	if(connect(sock_fd, result->ai_addr, result->ai_addrlen) == -1){
+                perror("connect");
+                exit(2);
+        }
+
+	//char *buffer = "This is Catherine talking. Hey Phoebe r u there?";
+
+        // For this trivial demo just assume write() sends all bytes in one go and is not interrupted
+	char filename[10];
+	read(sock_fd, filename,10);
+	int fd = open(filename, O_RDONLY);
+	char r[20];
+	read(fd, r, 19);
+	r[20] = '\0'; 
+	write(sock_fd, r, strlen(r));
+
+
+    return 0;
 }
